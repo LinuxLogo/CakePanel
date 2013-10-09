1c409dd985430145b4b2a4bf79f32356243f6f0e
Michael Yoo <sekjun9878@gmail.com>
2013-10-09 23:55:51 +1030

    Directory Restructure

--- src/sekjun9878/CakePanelBundle/sekjun9878CakePanelBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace sekjun9878\CakePanelBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class sekjun9878CakePanelBundle extends Bundle
-{
-}
--- src/sekjun9878/InvoiceBundle/sekjun9878InvoiceBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace sekjun9878\InvoiceBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class sekjun9878InvoiceBundle extends Bundle
-{
-}
